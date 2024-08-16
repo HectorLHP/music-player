18731fdcf2c3015526ac08b9b155a0a4c981ca39
Hector <rankecillo@gmail.com>
2024-08-16 11:58:33 -0600

    Final commit

--- script.js
+++ script.js
@@ -63,6 +63,19 @@ function nextSong() {
   playSong();
 }
 
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+}
+
+function setProgress(e) {
+  const width = e.target.clientWidth;
+  const clickX = e.offsetX;
+  const duration = audio.duration;
+  audio.currentTime = (clickX / width) * duration;
+}
+
 playBtn.addEventListener('click', () => {
   const isPlaying = musicContainer.classList.contains('play');
 
@@ -77,3 +90,4 @@ prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
 
 audio.addEventListener('timeupdate', updateProgress);
+progressContainer.addEventListener('click', setProgress);
